cf700b675f3fa4646776106b16149c4cbdf89f36
Avaco Sistemas <avacosistemas@gmail.com>
2024-03-08 11:04:01 -0300

    configuraicon ambiente test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,39 +1,18 @@
-export const PREFIX_DOMAIN_API =  'https://cpauorgapi-test.azurewebsites.net/api/';
-export const PREFIX_INSTITUCIONAL = 'https://test.cpau.org';
+export const PREFIX_DOMAIN_API =  'http://premec.ddns.net:48080/ws-rest-test/';
+
 
 export const environment = {
     localAuth: true,
-    
     production: true,
     hmr: false,
 
     /** AUTHENTICATION */
-    AUTHENTICATION_URL:  PREFIX_DOMAIN_API + 'User/login',
-    AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + 'Auth/refresh',
+    AUTHENTICATION_URL:  PREFIX_DOMAIN_API + 'auth',
+    AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + 'refresh',
 
     /* Config General URl*/
     URL_ROOT: '',
-    URL_LOGIN: PREFIX_INSTITUCIONAL,
+    URL_LOGIN: '/',
     // Business
-
-    URL_SECCIONES: 'seccion',
-    URL_CONTENIDOS: 'contenido',
-    URL_IMAGEN: 'imagen',
-    URL_BOLETIN: 'boletin',
-    URL_INDICE: 'indice',
-    URL_MAILING: 'mailing',
-    URL_FORMULARIO: 'formulario',
-    URL_ABM_MODAL: 'modalHome',
-    URL_PUBLICIDAD: 'bannerCampaing',
-    URL_PRODUCTO_EXTERNO : 'productoExterno',
-    URL_NOTICIA_CARROUSEL : 'noticiaCarrousel',
-    URL_LOGOUT_API: PREFIX_DOMAIN_API + 'user/logout',
-    URL_PERFIL_IDENTIFICACION: 'perfilIdentificacion',
-    URL_IDENTIFICACION_BUSQUEDA: 'identificacionBusqueda',
-    URL_DOMICILIOS: 'domicilio',
-    URL_MESSAGE_TEMPLATE: 'messageTemplate',
-	URL_IMAGEN_SUBIR: 'perfilMatriculadoUpload',
-	URL_EMAIL_ACCOUNT: 'emailAccount',
-    URL_ABM_ROLES: 'abmrole',
     AUTOCOMPLETE_WAITING_TIME: 700
 };
